5d6ed932252f699609d780d23934fa5f2ca0b2cb
xianeml <xianemml@gmail.com>
2021-09-21 04:13:25 +0900

    triggering events

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -7,6 +7,9 @@
         <li data-test="todo">{{ todo.text }}</li>
       </ul>
     </div>
+    <form data-test="form" @submit.prevent="createTodo">
+      <input data-test="new-todo" v-model="newTodo" />
+    </form>
   </div>
 </template>
 
@@ -15,11 +18,18 @@ export default {
   name: 'TodoApp',
   data() {
     return {
+      newTodo: '',
       todos: [
         { id: 1, text: 'sleep well', completed: false },
         { id: 2, text: 'eat well', completed: false },
       ],
     }
+  },
+  methods: {
+    createTodo() {
+      this.todos.push({ id: 3, text: this.newTodo, completed: false })
+      this.newTodo = ''
+    },
   },
 }
 </script>
--- tests/unit/TodoApp.spec.js
+++ tests/unit/TodoApp.spec.js
@@ -9,4 +9,16 @@ describe('TodoApp.vue', () => {
     const todo = wrapper.get('[data-test="todo"]')
     expect(todo.text()).toBe('sleep well')
   })
+
+  it('should add new todo', async () => {
+    // 모든 테스트 블록에서 컴포넌트 마운트 먼저 해줘야함
+    const wrapper = mount(TodoApp)
+    
+    expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(2)
+
+    await wrapper.get('[data-test="new-todo"]').setValue('New Todo')
+    await wrapper.get('[data-test="form"]').trigger('submit') // 아주 많은 이벤트를 트리거할 수 있다
+
+    expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(3)
+  })
 })
