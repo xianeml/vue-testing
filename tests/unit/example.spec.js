a033973bc53f6a8e32297f648fc422d54b7e7bac
xianeml <xianemml@gmail.com>
2021-09-21 03:35:46 +0900

    test structure

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,12 +2,22 @@ import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
-     // this is test name
+  /* 
+    we can have multiple testing blocks in this component
+    it() : test block
+    params1: this is a test name. messaging
+    params2: callback
+  */
   it('renders props.msg when passed', () => {
     const msg = 'new message'
+
+    // component we want to test
+    // 마운트된 컴포넌트 객체 리턴
     const wrapper = shallowMount(HelloWorld, {
       props: { msg }
     })
+    // Assertion: 해당 컴포넌트에 프랍스가 정상적으로 전달되었는지 테스트
+    // 래퍼에 있는 텍스트에 msg랑 매치되는게 있냐
     expect(wrapper.text()).toMatch(msg)
   })
 })
